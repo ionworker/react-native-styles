e84039f27ef3cbfb173c362bb35192f31d0429a1
ionworker <koshovnik@gmail.com>
2020-03-27 14:53:47 +0200

    code formatted

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { default as text } from "./text-styles";
 import { default as icon } from "./icon-styles";
 import { default as margin } from "./margings";
 import { default as padding } from "./paddings";
-import { default as TestView } from './TestView';
+import { default as TestView } from "./TestView";
 
 export { color, text, icon, margin, padding, TestView };
--- text-styles.js
+++ text-styles.js
@@ -1,47 +1,47 @@
 export default {
   title0: {
     fontSize: 40,
-    lineHeight: 44,
+    lineHeight: 44
   },
   title1: {
     fontSize: 32,
-    lineHeight: 36,
+    lineHeight: 36
   },
   title2: {
     fontSize: 28,
-    lineHeight: 32,
+    lineHeight: 32
   },
   title3: {
     fontSize: 24,
-    lineHeight: 28,
+    lineHeight: 28
   },
   title4: {
     fontSize: 20,
-    lineHeight: 24,
+    lineHeight: 24
   },
   header: {
     fontSize: 18,
-    lineHeight: 22,
+    lineHeight: 22
   },
   body: {
     fontSize: 17,
-    lineHeight: 21,
+    lineHeight: 21
   },
   action: {
     fontSize: 15,
     lineHeight: 19,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
   subhead: {
     fontSize: 14,
-    lineHeight: 18,
+    lineHeight: 18
   },
   footnote: {
     fontSize: 13,
-    lineHeight: 17,
+    lineHeight: 17
   },
   caption: {
     fontSize: 12,
-    lineHeight: 14,
+    lineHeight: 14
   }
-}
+};
